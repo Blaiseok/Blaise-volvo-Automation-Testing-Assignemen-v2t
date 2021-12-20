a2a2226250b6263c1eaec993be10a7b2e35824e4
Blaise <laco2020@gmail.com>
2021-12-20 19:19:48 +0000

    Add files via upload

---
+++ examples/wdio/custom-service/mocha.test.js
@@ -0,0 +1,8 @@
+describe('webdriver.io page', () => {
+    it('should be a pending test')
+
+    it('should have the right title', () => {
+        browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more')
+        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO')
+    })
+})
